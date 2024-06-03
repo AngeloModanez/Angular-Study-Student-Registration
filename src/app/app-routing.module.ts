08800ae9d1b04c0e920a72c6089623aeb0b5064a
Bananadev3 <angelomodanez30@gmail.com>
2024-06-02 23:21:06 -0300

    RouterLink and Navbar/Footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { BooksComponent } from './books/books.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'authors', component: AuthorsComponent},
+  {path: 'books', component: BooksComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
